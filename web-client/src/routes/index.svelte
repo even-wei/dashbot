639fefb6997772f15832cb7e9482bad141c3f88d
evenpeng <shinycockorach@gmail.com>
2019-09-30 23:39:20 +0800

    :sparkles: Replace webpack with rollup and integrate tailwind

--- web-client/src/routes/index.svelte
+++ web-client/src/routes/index.svelte
@@ -1,5 +1,5 @@
 <style>
-	h1 {
+	h1, figure, p {
 		text-align: center;
 		margin: 0 auto;
 	}
@@ -11,6 +11,20 @@
 		margin: 0 0 0.5em 0;
 	}
 
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 4em;
@@ -19,7 +33,18 @@
 </style>
 
 <svelte:head>
-	<title>DashBot</title>
+	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Welcome to DashBot!</h1>
+<h1>Great success!</h1>
+
+<figure>
+	<img alt='Borat' src='great-success.png'>
+	<figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+</button>
