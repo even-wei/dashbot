b26b84987975d10d08be2993e7f3ba0f330470d7
evenpeng <shinycockorach@gmail.com>
2019-09-30 19:17:17 +0800

    :sparkles: Add web client template using Sapper

---
+++ web-client/src/routes/index.svelte
@@ -0,0 +1,25 @@
+<style>
+	h1 {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>DashBot</title>
+</svelte:head>
+
+<h1>Welcome to DashBot!</h1>
